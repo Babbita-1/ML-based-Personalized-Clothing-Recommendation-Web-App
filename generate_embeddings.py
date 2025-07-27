ef49bae391e554090ded32cec64bda2b433bc2d1
Babita <bistababita17@gmail.com>
2025-07-27 22:22:05 +0545

    added

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -17,7 +17,7 @@ texts = (
     data['usage'].fillna('')
 ).tolist()
 
-# Load Hugging Face model
+# Load Hugging Face models
 model_name = 'sentence-transformers/all-MiniLM-L6-v2'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
@@ -29,7 +29,7 @@ def mean_pooling(model_output, attention_mask):
     return torch.sum(token_embeddings * input_mask_expanded, 1) / \
            torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
-# Generate theadded embeddings
+# Generate the embeddings
 embeddings = []
 for text in texts:
     encoded_input = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
