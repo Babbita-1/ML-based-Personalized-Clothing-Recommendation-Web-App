854f37372b0ed46559dbf7a1318a60826d96f431
Babita <bistababita17@gmail.com>
2025-07-27 22:09:25 +0545

    added

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sentence_transformers import SentenceTransformer
 
 
 # -------------------------
-# Recommending Class
+# Recommendings Class
 # -------------------------
 class EcommerceRecommendationSystem:
     def __init__(self):
