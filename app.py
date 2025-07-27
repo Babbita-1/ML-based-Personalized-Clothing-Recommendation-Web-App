f3a307c92c34fe66302dd2794d36d15e0b6d3481
Babita <bistababita17@gmail.com>
2025-07-27 22:24:37 +0545

    added

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 
 ###
-#####
+##
+###add
 # -------------------------
 # Recommenders Class
 # -------------------------
